3e3bca175c06809fc1deb0fc558347ff3c722e46
GengGeng026 <danielngkahking@gmail.com>
2024-06-18 14:25:41 +0800

    update only

---
+++ .history/Temp_20240618132623.py
@@ -0,0 +1,13 @@
+import os
+
+# 假設 GOOGLE_CALENDAR_CREDENTIALS_LOCATION 是從環境變量中獲取的
+GOOGLE_CALENDAR_CREDENTIALS_LOCATION = os.getenv("GOOGLE_CALENDAR_CREDENTIALS_LOCATION")
+
+if GOOGLE_CALENDAR_CREDENTIALS_LOCATION is None:
+    print("錯誤：未設置 GOOGLE_CALENDAR_CREDENTIALS_LOCATION 環境變量。")
+else:
+    # 如果 GOOGLE_CALENDAR_CREDENTIALS_LOCATION 已設置，接下來的代碼可以安全執行
+    if os.path.exists(GOOGLE_CALENDAR_CREDENTIALS_LOCATION):
+        print("文件存在。")
+    else:
+        print("文件不存在。")
